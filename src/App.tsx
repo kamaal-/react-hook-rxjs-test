71bf17d2701d5cbb578ffc44f92156984a7cf286
Kamaal ABOOTHALIB <kamaal@kamaal.me>
2019-08-27 13:39:48 +0400

    App updated.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Button from './components/button/Button'
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      Hello
+      <Button/>
     </div>
   );
 }
