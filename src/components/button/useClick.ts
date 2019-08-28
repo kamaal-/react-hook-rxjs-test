b946da7e62c1e949ed72800310c3b9dba20b5fb4
Kamaal ABOOTHALIB <kamaal@kamaal.me>
2019-08-28 20:00:26 +0400

    Cleanup subscription.

--- src/components/button/useClick.ts
+++ src/components/button/useClick.ts
@@ -1,10 +1,11 @@
 import React, { useRef, useEffect, useCallback, useState, RefObject, Dispatch} from 'react'
-import {fromEvent, Observable} from 'rxjs'
+import {fromEvent, Observable, Subscribable, Unsubscribable} from 'rxjs'
 import {map, debounceTime} from 'rxjs/operators'
-
 
 type NullableObservarbel = Observable<any> | null;
 type NUllabe = HTMLButtonElement | null;
+type NullableSubscribable = Subscribable<any> | null
+type NullableUnsubscribable = Unsubscribable | null
 export type Result = {
     ref: RefObject<HTMLButtonElement>;
     count:number;
@@ -19,16 +20,20 @@ export const useClick = (time:number = 500):Result => {
     const button: RefObject<HTMLButtonElement> = useRef(null)
     const [count, updateCount] = useState<number>(0)
     const fireAfterSubscribe = useCallback((c) => {updateCount(c)}, [])
-    useEffect(() => {
+    useEffect(():()=>void => {
         const el = button.current
         const observerble =  makeObservable(el, 'click')
         let _count = count
+        let subscribable:NullableSubscribable = null
+        let subscribe:NullableUnsubscribable = null
         if(observerble){
-            observerble.pipe(
+            subscribable = observerble.pipe(
                 map(e => _count++),
                 debounceTime(time)
-            ).subscribe(fireAfterSubscribe)
+            )
+            const subscribe = subscribable.subscribe(fireAfterSubscribe)
         }
+        return () => subscribe && subscribe.unsubscribe()
     // eslint-disable-next-line
     }, [])
     return {ref:button, count, updateCount:fireAfterSubscribe}
